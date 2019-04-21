6e380d069248fb118da333ce72f2b638b7741747
IsidroVale <isiperez1502@hotmail.com>
2019-04-21 17:10:40 -0400

    trabajo de estudio 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { LOCALE_ID } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    // { provide:LOCALE_ID, useValue:"es" }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
